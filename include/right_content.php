c51f9c67491b64f29b636238aa6be74d44282b3a
Seto El Kahfi <setoelkahfi@gmail.com>
2013-08-09 18:54:11 -0700

    Fix right widget

--- include/right_content.php
+++ include/right_content.php
@@ -33,39 +33,15 @@
 			<div id="f">           
 				<div class='desc_prod_box_big'>
 
-<iframe src="//www.facebook.com/plugins/likebox.php?href=http%3A%2F%2Fwww.facebook.com%2Fservice.komputer.laptop.tangerang&amp;width=170&amp;height=395&amp;colorscheme=light&amp;show_faces=false&amp;border_color&amp;stream=true&amp;header=false&amp;appId=224227540922551" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:170px; height:395px;" allowTransparency="true"></iframe>
+<iframe src="//www.facebook.com/plugins/likebox.php?href=http%3A%2F%2Fwww.facebook.com%2Ftokonano&amp;width=170&amp;height=395&amp;colorscheme=light&amp;show_faces=false&amp;border_color&amp;stream=true&amp;header=false&amp;appId=224227540922551" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:170px; height:395px;" allowTransparency="true"></iframe>
 </div><br>
 			</div><!--end tabs-1-->
 			<div id="t">
 				<div class='rev_prod_box_big'>
-				<script charset="utf-8" src="http://widgets.twimg.com/j/2/widget.js"></script>
-<script>
-new TWTR.Widget({
-  version: 2,
-  type: 'profile',
-  rpp: 4,
-  interval: 30000,
-  width: 'auto',
-  height: 300,
-  theme: {
-    shell: {
-      background: '#ab9eab',
-      color: '#ffffff'
-    },
-    tweets: {
-      background: '#291029',
-      color: '#ffffff',
-      links: '#1b8a9e'
-    }
-  },
-  features: {
-    scrollbar: false,
-    loop: false,
-    live: false,
-    behavior: 'default'
-  }
-}).render().setUser('Sengkoeloen').start();
-</script>
+<a class="twitter-timeline" href="https://twitter.com/SetoElKahfi" data-widget-id="483817050393952256">Tweet oleh @SetoElKahfi</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+
+
 				</div><!--end rev_prod_box_big-->          
 			</div><!--end tabs-2-->      
             <div id="y">
